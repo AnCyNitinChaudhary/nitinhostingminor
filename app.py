672352dde0c46e58c67538465a2dce0805d822f8
Nitin Chaudhary <109977558+AnCyNitinChaudhary@users.noreply.github.com>
2024-04-25 23:59:47 +0530

    for hosting

---
+++ app.py
@@ -0,0 +1,10 @@
+import sys
+import json
+import cv2
+video_capture = cv2.VideoCapture(0)
+ret,frame=video_capture.read()
+
+face_names = ['9921103118']
+output_json = json.dumps({"face_name": face_names})
+print(output_json, flush=True)  # Print JSON object to stdout
+
