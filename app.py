3e2d2f92ad9a0b1464aaeaccaabfba4100d9b163
Nitin Chaudhary <109977558+AnCyNitinChaudhary@users.noreply.github.com>
2024-04-26 00:51:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-import cv2
-video_capture = cv2.VideoCapture(0)
-ret,frame=video_capture.read()
-
 face_names = ['9921103118']
 output_json = {"face_name": face_names}
 print(output_json, flush=True)  # Print JSON object to stdout
